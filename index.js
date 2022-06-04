f066a697cdd9daa6be3f855082a074f4a2a64661
Aldi Andika <aldi.andika@gmail.com>
2022-06-04 22:34:23 +0700

    Turn in challenge 5

--- index.js
+++ index.js
@@ -6,15 +6,31 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (nameList, callback) => {
+
+  // Get sorted array based on callback fn
+  let sortedArr = callback(nameList);
+
+  // Format array
+  let formattedArr = [];
+  for (let item=0; item < sortedArr.length; item++){
+    formattedArr.push((item+1) + ". " + sortedArr[item]);
+  }
+
+  return formattedArr;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (nameList) => {
+  return nameList.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (nameList) => {
+  return nameList.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
